18a053f5f11fa63e692e724f35b0f3c6951c1b29
Siganov Ilya <ilya.blan4@gmail.com>
2014-11-16 13:15:19 +0600

    typo fix xfce crash

--- lib/hexapic/desktop_environment.rb
+++ lib/hexapic/desktop_environment.rb
@@ -25,18 +25,17 @@ module Hexapic
       
       DE[wm_name] || wm_name.to_sym
     end    
-  end
+    def self.output
+      output_name = nil
 
-  def self.output
-    output_name = nil
+      Open3.popen3('xrandr' ,'| grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/"') do |inp, out, err|
+        inp.close
+        err.close
+        output_name = out.strip
+        out.close
+      end
 
-    Open3.popen3('xrandr' ,'| grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/"') do |inp, out, err|
-      inp.close
-      err.close
-      output_name = out.strip
-      out.close
+      output_name
     end
-
-    output_name
   end
 end
--- lib/hexapic/version.rb
+++ lib/hexapic/version.rb
@@ -1,3 +1,3 @@
 module Hexapic
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
